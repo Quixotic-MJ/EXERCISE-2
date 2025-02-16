a38df0933daf26458a392c3a3d1422806fe96401
Magdasal John Mark P. <magdasaljohnmark29@gmail.com>
2025-02-16 18:40:13 +0800

    commit 2
    Revised the redundant codes

--- Exercise2.php
+++ Exercise2.php
@@ -10,50 +10,61 @@
 
 <body>
     <?php
-    $cities = ["Tokyo",
-    "Mexico City", 
-    "New York City", 
-    "Mumbai", 
-    "Seoul", 
-    "Shanghai", 
-    "Lagos", 
-    "Buenos Aires", 
-    "Cairo", 
-    "London"];
+    $cities = [
+        "Tokyo",
+        "Mexico City",
+        "New York City",
+        "Mumbai",
+        "Seoul",
+        "Shanghai",
+        "Lagos",
+        "Buenos Aires",
+        "Cairo",
+        "London"
+    ];
 
-    $add = [ "Los Angeles", 
-    "Calcutta", 
-    "Osaka", 
-    "Beijing"];
+
+    function printCities($cities)
+    {
+        foreach ($cities as $city) {
+            echo "$city ,";
+        }
+    }
+
+    function sortAndPrint($cities)
+    {
+        sort($cities);
+
+        echo "<ul>";
+        foreach ($cities as $city) {
+            echo "<li>$city</li>";
+        }
+        echo "</ul>";
+    }
+
+    function addCities(&$cities){
+        $add = [
+            "Los Angeles",
+            "Calcutta",
+            "Osaka",
+            "Beijing"
+        ];
+        
+        $cities = array_merge($add, $cities);
+    }
 
     echo "<h1>PHP Exercise 2</h1>";
 
     echo "<h4>Largest cities: </h4>";
-    foreach ($cities as $city){
-        echo "$city ,";
-    }
-
-    sort($cities);
+    printCities($cities);
 
     echo "<h4>Sorted cities: </h4>";
-    echo "<ul>";
-    foreach ($cities as $city){
-        echo "<li>$city</li>";
-    }
-    echo "</ul>";
-
-    foreach ($add as $new){
-        $cities[] = $new;
-    }
-
-    sort($cities);
-
+    sortAndPrint($cities);
+    
     echo "<h4>New sorted cities: </h4>";
-    echo "<ul>";
-    foreach ($cities as $city){
-        echo "<li>$city</li>";
-    }
-    echo "</ul>";
+    addCities($cities);
+    sortAndPrint($cities);
+
     ?>
 </body>
 
